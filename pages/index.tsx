4cdd99abd7720955735bc5ea2dfe3dd77a066677
salmanArtadi <49398513+salmanArtadi@users.noreply.github.com>
2025-08-02 21:49:18 +0700

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+// ================================================
+// File: /pages/index.tsx
+// ================================================
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/login');
+  }, [router]);
+
+  return null;
+}
